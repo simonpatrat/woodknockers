23bc949b2db379c118595f82fb0eb07cbf8debb9
simonpatrat <simonpatrat@users.noreply.github.com>
2015-11-08 19:12:04 +0100

    First commit

---
+++ js/site.js
@@ -0,0 +1,85 @@
+(function( $ ){
+    
+        /// Video Players
+        window.onload = function() {
+
+        // Video
+        var video = document.getElementById("video-player");
+
+        // Buttons
+        var playButton = document.getElementById("play-pause");
+        var muteButton = document.getElementById("mute");
+        var fullScreenButton = document.getElementById("full-screen");
+
+        // Sliders
+        var seekBar = document.getElementById("seek-bar");
+        var volumeBar = document.getElementById("volume-bar");
+
+        // Event listener for the play/pause button
+        playButton.addEventListener("click", function() {
+          if (video.paused == true) {
+            // Play the video
+            video.play();
+
+            // Update the button text to 'Pause'
+            playButton.innerHTML = "Pause";
+          } else {
+            // Pause the video
+            video.pause();
+
+            // Update the button text to 'Play'
+            playButton.innerHTML = "Play";
+          }
+        });
+
+        }
+
+    //$('#site-navigation, #site-header, .site-container').addClass('invisible');
+    $(document).ready(function() {
+        $('.loader-container').addClass('loaded');
+        $(".loader-container").one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', 
+        function() {
+         $(this).hide();
+/*        setTimeout(function(){
+            $('#site-navigation, #site-header, .site-container').removeClass('invisible');
+        },500);*/
+            // Masonry showcase
+            $('.showcase-grid-inner').masonry({
+              // options
+              itemSelector: '.sc-grid-bloc',
+              columnWidth: '.grid-sizer',     
+            });
+
+            // Flickity carousel
+            $('.showcase-carousel .gallery').flickity({
+                cellAlign: 'left',
+                contain: true
+            });
+            $('.featured-products-gallery').flickity({
+                cellAlign: 'center',
+                contain: true
+            });
+         
+        });
+        
+
+        
+        
+        
+        headerH = $('.site-header').height();
+        halfHeaderH = headerH /2;
+        $(window).scroll(function(){
+            w = $(this);
+            wTop = w.scrollTop();
+            
+            if(wTop> halfHeaderH){
+                $('#site-navigation').addClass('minified');
+            }else{
+                $('#site-navigation').removeClass('minified');
+            }
+        });
+ 
+    });
+
+})( jQuery );
+
